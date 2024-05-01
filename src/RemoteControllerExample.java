035afa29e21d15f93f971977a16baffa45ea29a1
newstar <maori36@naver.com>
2024-05-01 21:10:43 +0900

    24.04.28

---
+++ src/RemoteControllerExample.java
@@ -0,0 +1,7 @@
+public class RemoteControllerExample {
+    public static void main(String[] args) {
+        RemoteController rc = null;
+        rc = new Radio(); // 다형성
+        rc = new Television();
+    }
+}
---
+++ src/Television.java
@@ -0,0 +1,22 @@
+public class Television implements RemoteController {
+    private int volume = 0;
+
+    @Override
+    public void turnOn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'turnOn'");
+    }
+
+    @Override
+    public void turnOff() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'turnOff'");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVolume'");
+    }
+
+}
